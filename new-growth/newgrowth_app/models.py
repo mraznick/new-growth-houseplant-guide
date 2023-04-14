89b30f5384d0e0d245c747706c011d2ef7acced6
Mitch Raznick <mraznick@gmail.com>
2023-04-14 16:15:56 -0400

    fixed boolean fields in models

--- new-growth/newgrowth_app/models.py
+++ new-growth/newgrowth_app/models.py
@@ -6,8 +6,8 @@ class User_profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, default=4)
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
-    travel_freq = models.BooleanField
-    pets = models.BooleanField
+    travel_freq = models.BooleanField(('Travels Frequently'), default=False)
+    pets = models.BooleanField(('Has Pets'), default=False)
     windows = models.CharField(
         max_length=50, default='Enter Cardinal Direction')
     # Need to figure out how to add other user model info in road map
@@ -23,5 +23,5 @@ class Plant(models.Model):
     # Not sure whether this is the correct method
     img = models.URLField(max_length=200)
     window_pref = models.CharField(max_length=50)
-    drought_tol = models.BooleanField
-    toxic = models.BooleanField
+    drought_tol = models.BooleanField(('Drought Tolerant'), default=False)
+    toxic = models.BooleanField(('Toxic'), default=False)
