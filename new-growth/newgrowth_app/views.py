d49db52106c76c88ad2c6121e7b3ffa8e1a407dc
Mitch Raznick <mraznick@gmail.com>
2023-04-17 16:48:21 -0400

    started newgrowth_app views.py

--- new-growth/newgrowth_app/views.py
+++ new-growth/newgrowth_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User_profile, Plant
+from rest_framework.views import APIView
+from .serializers import User_profileSerializer, PlantSerializer
+from rest_framework import permissions
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UserProfile_ViewSet(viewsets.ModelViewSet):
+    queryset = User_profile.objects.all()
+    serializer_class = User_profileSerializer
+
+
+class Plant_ViewSet(viewsets.ModelViewSet):
+    queryset = Plant.objects.all()
+    serializer_class = PlantSerializer
