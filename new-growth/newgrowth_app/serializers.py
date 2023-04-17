62dcacd5ea8d883c99d9f946b2e661d857948641
Mitch Raznick <mraznick@gmail.com>
2023-04-17 15:40:11 -0400

    built out newgrowth_app serializers

---
+++ new-growth/newgrowth_app/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Plant, User_profile
+
+
+class User_profileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User_profile
+        fields = '__all__'
+
+
+class PlantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plant
+        fields = '__all__'
+
+
+class PlantSerializer(serializers.BaseSerializer):
+    def to_representation(self, instance):
+        return {
+            "id": instance.id,
+            # might need to be instance.nick.name
+            "nickname": instance.nickname,
+            "latin name": instance.latin.name,
+            "img": instance.img,
+            "window preference": instance.window.pref,
+            "drought tolerance": instance.drought.tol,
+            "toxic": instance.toxic
+        }
