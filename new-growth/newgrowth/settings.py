1e6be737ffe0f5cc82da0ed02bde0846b18ada71
Mitch Raznick <mraznick@gmail.com>
2023-04-20 14:40:38 -0400

    model notes and settings fix

--- new-growth/newgrowth/settings.py
+++ new-growth/newgrowth/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'newgrowth_app',
     'user_auth',
     'rest_framework',
-    'knox'
 ]
 
 MIDDLEWARE = [
--- new-growth/newgrowth_app/models.py
+++ new-growth/newgrowth_app/models.py
@@ -8,6 +8,7 @@ class User_profile(models.Model):
     email = models.CharField(max_length=50)
     travel_freq = models.BooleanField(('Travels Frequently'), default=False)
     pets = models.BooleanField(('Has Pets'), default=False)
+    # Switch to multi-choice checkboxes? Consider how to parse out (convert to array etc)
     windows = models.CharField(
         max_length=50, default='Enter Cardinal Direction')
     # Need to figure out how to add other user model info in road map
@@ -22,6 +23,7 @@ class Plant(models.Model):
     latin_name = models.CharField(max_length=50)
     # Not sure whether this is the correct method
     img = models.URLField(max_length=200)
+    # Switch to multi-choice checkboxes? Consider how to parse out (convert to array etc)
     window_pref = models.CharField(max_length=50)
     drought_tol = models.BooleanField(('Drought Tolerant'), default=False)
     toxic = models.BooleanField(('Toxic'), default=False)
