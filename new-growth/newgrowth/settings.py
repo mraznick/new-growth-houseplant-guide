c654f6429dda56ddaf1d127538ff284b448e3511
Mitch Raznick <mraznick@gmail.com>
2023-04-20 15:54:45 -0400

    updated allowed_hosts in settings

--- new-growth/newgrowth/settings.py
+++ new-growth/newgrowth/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-m@st4glqy+)wz^p6!v5^kh7-5r^xi57yt$_%_i1c5%dh-hgrpn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'newgrowth.netlify.app']
 
 
 # Application definition
