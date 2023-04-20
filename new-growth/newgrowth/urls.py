2a22733e00acdac01b8254b2336b0f665d13e019
Mitch Raznick <mraznick@gmail.com>
2023-04-20 14:49:38 -0400

    url paths

--- new-growth/newgrowth/urls.py
+++ new-growth/newgrowth/urls.py
@@ -24,5 +24,8 @@ router.register(r'profiles', UserProfile_ViewSet)
 router.register(r'plant', Plant_ViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('', include('newgrowth_app.urls')),
+    path('user/', include('user_auth.urls')),
     path('admin/', admin.site.urls),
 ]
