2c8bb8573552382631d19ed521e546255085b189
Mitch Raznick <mraznick@gmail.com>
2023-04-20 15:55:58 -0400

    add whitenoise to installed apps and middleware, added corse to middleware

--- new-growth/newgrowth/settings.py
+++ new-growth/newgrowth/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['localhost', 'newgrowth.netlify.app']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,6 +48,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
