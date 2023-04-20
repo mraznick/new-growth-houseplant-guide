a6394cdf320fc570f39104d2915667871831488b
Mitch Raznick <mraznick@gmail.com>
2023-04-20 15:01:18 -0400

    user profile update viewset

--- new-growth/newgrowth_app/views.py
+++ new-growth/newgrowth_app/views.py
@@ -14,3 +14,33 @@ class UserProfile_ViewSet(viewsets.ModelViewSet):
 class Plant_ViewSet(viewsets.ModelViewSet):
     queryset = Plant.objects.all()
     serializer_class = PlantSerializer
+
+
+class User_Profile_Update_ViewSet(APIView):
+    def put(self, request, id):
+        try:
+            user = self.request.user
+            isAuthenticated = user.is_authenticated
+            if isAuthenticated:
+                name = request.data['name']
+                email = request.data['email']
+                travel_freq = request.data['travel_freq']
+                pets = request.data['pets']
+                windows = request.data['windows']
+                # May need to rework following lines
+                reqProfile = User_profile.objects.get(user=user)
+                logProfile = User_profile.objects.get(id=id)
+                userProfile = reqProfile.id
+                loggedProfile = logProfile.id
+                print(userProfile)
+                print(loggedProfile)
+                if loggedProfile == userProfile:
+                    User_profile.objects.update(
+                        name=name, email=email, travel_freq=travel_freq, pets=pets, windows=windows)
+                    return Response({'message': "Profile updated successfully."})
+                else:
+                    return Response({'message': "You are not authorized to perform this action."})
+            else:
+                return Response({'error': "Not Authenticated - Please make sure you include a token."})
+        except:
+            return Response({'error': "Error: Invalid"})
