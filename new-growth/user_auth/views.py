8a0db10de146aeeb779df98b56b2f8520c037d43
Mitch Raznick <mraznick@gmail.com>
2023-04-20 15:28:22 -0400

    user_auth views imports

--- new-growth/user_auth/views.py
+++ new-growth/user_auth/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from django.contrib import auth
+from django.shortcuts import render
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from rest_framework import permissions
+# from knox.models import AuthToken
+from newgrowth_app.models import User_profile, Plant
+from newgrowth_app.serializers import User_profileSerializer, PlantSerializer
