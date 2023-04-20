57a57f5e80b13b2a5510fde592a962bc5aa19ce1
Mitch Raznick <mraznick@gmail.com>
2023-04-20 15:14:00 -0400

    single plant viewset; also wrote post-MVP viewset allowing users to add plants, commented out

--- new-growth/newgrowth_app/views.py
+++ new-growth/newgrowth_app/views.py
@@ -44,3 +44,50 @@ class User_Profile_Update_ViewSet(APIView):
                 return Response({'error': "Not Authenticated - Please make sure you include a token."})
         except:
             return Response({'error': "Error: Invalid"})
+
+
+class OnePlant_ViewSet(APIView):
+    permission_classes = [
+        permissions.AllowAny
+    ]
+
+    def get(self, id):
+        try:
+            plant_results = Plant.objects.get(id=id)
+            plant = PlantSerializer(plant_results)
+            return Response({'plant': plant.data})
+        except:
+            return Response({'error': "Sorry, something went wrong"})
+
+#  FOR POST MVP - VIEWSET ALLOWING USERS TO ADD PLANTS
+# class AllPlant_ViewSet(APIView):
+#     permission_classes = [
+#         permissions.AllowAny
+#     ]
+
+#     def get(self, request):
+#         try:
+#             results = Plant.objects.all()
+#             all_plant = PlantSerializer(results, many=True)
+#             return Response(all_plant.data)
+#         except:
+#             return Response({'error': "Something went wrong"})
+
+#     def post(self, request):
+#         try:
+#             user = self.request.user
+#             isAuthenticated = user.is_authenticated
+#             if isAuthenticated:
+#                 nickname = request.data['nickname']
+#                 latin_name = request.data['latin_name']
+#                 img = request.data['img']
+#                 window_pref = request.data['window_pref']
+#                 drought_tol = request.data['drought_tol']
+#                 toxic = request.data['toxic']
+#                 userProfile = User_profile.objects.get(user=user)
+#                 Plant.objects.create(user=userProfile, nickname=nickname, latin_name=latin_name, img=img, window_pref=window_pref, drought_told=drought_tol, toxic=toxic)
+#                 return Response({'message': "Plant successfully added! THank you for your contribution."})
+#             else:
+#                 return Response({'error': "Not Authenticated - Please make sure you include a token."})
+#         except:
+#             return Response({'error': "Error: Invalid"})
