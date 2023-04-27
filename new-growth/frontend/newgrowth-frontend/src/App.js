1a1e6cf6d71167c5d01d214fd815a96454172072
Mitch Raznick <mraznick@gmail.com>
2023-04-26 20:46:54 -0400

    temporary app.js setup for landing page

--- new-growth/frontend/newgrowth-frontend/src/App.js
+++ new-growth/frontend/newgrowth-frontend/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Signup from './Screens/Signup';
+import {getUser} from './services/users.js'
 
-function App() {
+export default function App() {
+  // const [user, setUser] = useState(null);
+  // const [toggle, setToggle] = useState(false);
+
+  // useEffect(() => {
+  //   const fetchUser = async () => {
+  //     let response = await getUser()
+  //     setUser(response)
+  //   }
+  //   fetchUser()
+  // }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="newgrowth">
+      <h1>Welcome to New Growth</h1>
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Signup setUser={setUser} />} />
+          <Route path="/" element={<Home toggle={toggle} user={user} />} />
+        </Routes>
+      </BrowserRouter> */}
     </div>
   );
 }
 
-export default App;
+
